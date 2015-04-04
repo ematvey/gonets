774048c204888d7a0829fc276736416581287884
Matvey Ezhov <ematvey@ostrovok.ru>
2015-04-04 22:15:49 +0300

    Normalizer added

---
+++ utils.go
@@ -0,0 +1,23 @@
+package gonets
+
+import "errors"
+
+type Normalizer struct {
+	mean []float64
+	std  []float64
+}
+
+func NormalizerNew(mean, std []float64) (*Normalizer, error) {
+	if len(mean) != len(std) || len(mean) == 0 {
+		return nil, errors.New("incorrect lenghts")
+	}
+	return &Normalizer{mean, std}
+}
+
+func (n *Normalizer) Normalize(in []float64) (out []float64) {
+	out = make([]float64, len(in))
+	for i := range in {
+		out[i] = in[i]*n.std[i] + n.mean[i]
+	}
+	return
+}
